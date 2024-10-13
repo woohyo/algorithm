f7ed17510f078366145b8303dbea5f0bb970b800
Hyojin Park <68233870+prkhyo@users.noreply.github.com>
2024-10-13 11:33:15 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 12.13 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,16 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
         for(int i = 1; i < food.length; i++) {
             for(int j = 1; j <= food[i] / 2; j++) {
-                answer += i;
+                sb.append(i);
             }
         }
-        answer = new StringBuilder(answer)
-                .append('0')
-                .append(new StringBuilder(answer).reverse())
-                .toString();
         
-        return answer;
+        return sb + "0" + new StringBuilder(sb).reverse();
     }
 }
